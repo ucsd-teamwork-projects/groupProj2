ab16a2b9781793683bb6a622ca4c937c998564a9
Edel Marcelino <edelm8@gmail.com>
2019-10-02 22:24:26 -0700

    cleaned up code, added temp landing page

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,2 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
-
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+DROP DATABASE IF EXISTS interxtions_db;
+CREATE DATABASE interxtions_db;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,28 +6,12 @@ const { ensureAuthenticated } = require("../config/auth");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+    res.render("index");
   });
 
   //dashboard page
   app.get("/dashboard", ensureAuthenticated, (req, res) => {
-    res.render("dashboard")
+    res.render("dashboard", {name: req.user.firstname});
   });
   
   //login page
