8fc0b0bf8ad67ff39fb472659a4a68a0f7207681
tylerhipke <tyerhipke@gmail.com>
2019-10-03 21:55:23 -0700

    redo api call, begin initial call to find rxcui for search query

--- config/rxnorm.js
+++ config/rxnorm.js
@@ -1,25 +1,49 @@
+//https://rxnav.nlm.nih.gov/REST/rxcui/153165/allrelated 
+
+
 var axios = require("axios");
 
-rxNorm.rxcuiSearch(userSearch) {
-  var rxcui;
-  var userSearch;
-  
+var userSearch;
+var rxObject = {};
+
+
+rxSearch("lipitor");
+
+function rxSearch(userSearch) {
+
   console.log("Initiate rxNorm API call with the userSearch: " + userSearch);
-  var queryURL = "https://rxnav.nlm.nih.gov/REST/rxcui?name=" + userSearch;
+  var queryURL = "https://rxnav.nlm.nih.gov/REST/approximateTerm?term=" +
+    userSearch + "&maxEntries=1";
 
   axios
     .get(queryURL)
     .then(function (response) {
-      rxcui = response.data.idGroup.rxnormId[0];
+      var rxId = response.data.approximateGroup.candidate[0].rxcui;
+
+      getRxObject(rxId);
+
+      //take rxcui, axios get object with drug names
     })
     .catch(function (error) {
-      console.log("\n\n\nrxnorm.js has halted this process!\n\n\n");
+      console.log("\n\n\nrxnorm.js has halted this host process! RXCUI search failed to generate due to invalid query.\n\n\n");
       console.log(error);
     })
-    .finally(function () {
-      console.log("Call done.  Returning rxnormId: #" + rxcui);
-      return rxcui;
-    });
+    .finally(function () {});
 }
 
-module.exports = rxNorm;
+function getRxObject( rxcui ) {
+  var queryURL = "https://rxnav.nlm.nih.gov/REST/rxcui/" + rxcui + "/allrelated"
+
+axios
+  .get(queryURL)
+  .then(function (response) {
+    console.log("generating rx object ", response.data.allRelatedGroup.conceptGroup);
+
+    //take rxcui, axios get object with drug names
+  })
+  .catch(function (error) {
+    console.log("\n\n\nrxnorm.js has halted this process! RXCUI to name list conversion failed to generate due to invalid id.\n\n\n");
+    console.log(error);
+  })
+  .finally(function () {});
+}
