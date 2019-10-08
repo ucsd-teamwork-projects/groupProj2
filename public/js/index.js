c5f0ba8870cb01961f867e820b1dad44f537a294
Edel Marcelino <edelm8@gmail.com>
2019-10-07 18:44:23 -0700

    added: more info modal when rxNum is clicked

--- public/js/index.js
+++ public/js/index.js
@@ -1,141 +1,3 @@
-// Get references to page elements
-// var $exampleText = $("#example-text");
-// var $exampleDescription = $("#example-description");
-// var $submitBtn = $("#submit");
-// var $exampleList = $("#example-list");
-
-// // The API object contains methods for each kind of request we'll make
-// var API = {
-//   saveExample: function(example) {
-//     return $.ajax({
-//       headers: {
-//         "Content-Type": "application/json"
-//       },
-//       type: "POST",
-//       url: "api/examples",
-//       data: JSON.stringify(example)
-//     });
-//   },
-//   getExamples: function() {
-//     return $.ajax({
-//       url: "api/examples",
-//       type: "GET"
-//     });
-//   },
-//   deleteExample: function(id) {
-//     return $.ajax({
-//       url: "api/examples/" + id,
-//       type: "DELETE"
-//     });
-//   }
-// };
-
-// // refreshExamples gets new examples from the db and repopulates the list
-// var refreshExamples = function() {
-//   API.getExamples().then(function(data) {
-//     var $examples = data.map(function(example) {
-//       var $a = $("<a>")
-//         .text(example.text)
-//         .attr("href", "/example/" + example.id);
-
-//       var $li = $("<li>")
-//         .attr({
-//           class: "list-group-item",
-//           "data-id": example.id
-//         })
-//         .append($a);
-
-//       var $button = $("<button>")
-//         .addClass("btn btn-danger float-right delete")
-//         .text("ｘ");
-
-//       $li.append($button);
-
-//       return $li;
-//     });
-
-//     $exampleList.empty();
-//     $exampleList.append($examples);
-//   });
-// };
-
-// // handleFormSubmit is called whenever we submit a new example
-// // Save the new example to the db and refresh the list
-// var handleFormSubmit = function(event) {
-//   event.preventDefault();
-
-//   var example = {
-//     text: $exampleText.val().trim(),
-//     description: $exampleDescription.val().trim()
-//   };
-
-//   if (!(example.text && example.description)) {
-//     alert("You must enter an example text and description!");
-//     return;
-//   }
-
-//   API.saveExample(example).then(function() {
-//     refreshExamples();
-//   });
-
-//   $exampleText.val("");
-//   $exampleDescription.val("");
-// };
-
-// // handleDeleteBtnClick is called when an example's delete button is clicked
-// // Remove the example from the db and refresh the list
-// var handleDeleteBtnClick = function() {
-//   var idToDelete = $(this)
-//     .parent()
-//     .attr("data-id");
-
-//   API.deleteExample(idToDelete).then(function() {
-//     refreshExamples();
-//   });
-// };
-
-// // Add event listeners to the submit and delete buttons
-// $submitBtn.on("click", handleFormSubmit);
-// $exampleList.on("click", ".delete", handleDeleteBtnClick);
-
-// const search = document.getElementById("rxName");
-// const matchlist = document.getElementById("matchlist");
-
-// const searchMeds = async searchText => {
-//   const res = await fetch("https://rxnav.nlm.nih.gov/REST/rxcui.json?name="+searchText.toLowerCase());
-//   const meds = await res.json();
-
-//   let matches = [];
-
-//   if (meds.idGroup.rxnormId) {
-//     meds.idGroup.rxnormId.forEach(element => {
-//       matches.push(element);
-//     });
-//   }
-
-//   if (searchText.length ===0) {
-//     matches = [];
-//   }
-
-//   outputHtml(meds.idGroup.name, matches);
-// };
-
-// const outputHtml = (name, matches) => {
-//   if(matches.length > 0) {
-//     var upperName = name.toUpperCase();
-//     const html = matches.map(match => `
-//       <div data-rxNum="${match}" class="card card-body mb-4 medResult">
-//         <h4>${upperName}</h4>
-//         <small>Rx: ${match}</small>
-//       </div>
-//     `).join("");
-
-//     matchlist.innerHTML = html;
-//   }
-// }
-
-// search.addEventListener("input", () => searchMeds(search.value));
-
 $(function() {
   $(document).on("click", ".medResult", function() {
     $("#rxName").val($(this).attr("data-rxName"));
@@ -154,12 +16,39 @@ $(function() {
     });
   });
 
-  // $(document).on("click", ".rxNum", function() {
-  //   var rxNum = $(this).attr("data-rx");
-  //   console.log(rxNum)
-  //   var url = `https://rxnav.nlm.nih.gov/REST/RxTerms/rxcui/${rxNum}/allinfo.json`;
-  //   $.get(url, function(data) {
-  //     console.log(data)
-  //   })
-  // });
+  $(document).on("click", ".rxNum", function() {
+    var rxNum = $(this).attr("data-rx");
+    console.log(rxNum);
+    var url = `https://rxnav.nlm.nih.gov/REST/RxTerms/rxcui/${rxNum}/allinfo.json`;
+    $.get(url, function(data) {
+      var medInfo = data.rxtermsProperties;
+
+      if (medInfo === null) {
+        $("#modalTitle").text(rxNum);
+        $(".modal-body").append(
+          $("<p>").html("No information available for this RxCUI!")
+        );
+      } else {
+        $("#modalTitle").text(medInfo.fullName);
+        $(".modal-body").append(
+          $("<p>").html("<strong>Brand Name: </strong>" + medInfo.brandName)
+        );
+        $(".modal-body").append(
+          $("<p>").html(
+            "<strong>Generic RxCUI#: </strong>" + medInfo.genericRxcui
+          )
+        );
+        $(".modal-body").append(
+          $("<p>").html("<strong>Dose Route: </strong>" + medInfo.route)
+        );
+        $(".modal-body").append(
+          $("<p>").html("<strong>Strength: </strong>" + medInfo.strength)
+        );
+      }
+    });
+  });
+
+  $(document).on("click", ".close-modal", function() {
+    $(".modal-body").empty();
+  });
 });
